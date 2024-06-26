25aa42a83ef11c88d3006a201be4a348f347e2e3
Mundane-Editorial <uday2k17@rediffmail.com>
2024-06-26 12:06:41 +0530

    node js architecture

---
+++ 13-how-node-works/index.js
@@ -0,0 +1,12 @@
+console.log("starting up");
+
+setTimeout(()=>{
+    console.log("2 seconds have passed");
+},2000)
+
+setTimeout(()=>{    //settimeout function is an function of c/c++
+    console.log("0 seconds have passed");
+},0)
+
+
+console.log("finishing up");
